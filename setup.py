87a4093bbc2f5adefb6a0c40575f56028ec5e12d
Biprayan Choudhuri <biprayanchoudhuri58@gmail.com>
2025-10-08 02:51:33 +0530

    Initial commit and Experiments

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A data science tool that analyzes YouTube comments to detect sentiment and extract insights using natural language processing.',
+    author='BiprayaN Choudhuri',
+    license='',
+)
